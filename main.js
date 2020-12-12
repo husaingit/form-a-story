d475333a342b191865db1252d5b833f3c5ba3621
husaingit <60727385+husaingit@users.noreply.github.com>
2020-12-12 11:55:40 +0530

    Update main.js

--- main.js
+++ main.js
@@ -1,76 +1,66 @@
-function handleSubmit() {
-    const animal = document.getElementById('animal').value;
-    localStorage.setItem('animal', animal);
+<!DOCTYPE html>
+<html lang="en">
 
-    const anotherAnimal = document.getElementById("anotheranimal").value;
-    localStorage.setItem('anotheranimal', anotherAnimal);
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Story telling</title>
+    <link rel="stylesheet" href="style.css">
+</head>
 
-    const onemore = document.getElementById("onemore").value;
-    localStorage.setItem('onemore', onemore);
+<body>
+    <div class="main">
+        <div class="head">
 
-    const speed = document.getElementById("speed").value;
-    localStorage.setItem('speed', speed);
+            <h4>Form-A-Story</h4>
+        </div>
+        <div class="form-section">
+            <div class="form-heading">
+                <h2>The <u><span id="animal"></span></u> and the <u><b><span
+                                id="anotheranimal"></span></b></u></h2>
 
-    const num = document.getElementById("num").value;
-    localStorage.setItem('num', num);
+            </div>
+            <div class="form">
 
+                <p>
+                    A <u><b><span id="animal"> </span></b></u> was making fun of the <u><b><span id="anotheranimal"></span></b></u> one day for being slow.
+                </p>
 
-    const adjective = document.getElementById("adjective").value;
-    localStorage.setItem('adjective', adjective);
+                <p>
+                    "Do you ever get anywhere?" he asked with a mocking laugh.
+                </p>
 
-    const quotes = document.getElementById("quotes").value;
-    localStorage.setItem('quotes', quotes);
+                <p>
+                    <u><b><span id="decision"></span></b></u> replied the <u><b><span
+                                id="anotheranimal"></span></b></u>. "but i get there
+                    <u><b><span id="speed"></span></b></u> than you think. I'll run a race and prove it."
+                </p>
 
-    const verbs = document.getElementById("verbs").value;
-    localStorage.setItem('verbs', verbs);
+                <p>
+                    The <u><b><span id="animal"></span></b></u> was so much <u><b><span id="adjective"></span></b></u> at the idea of running a race with the <u><span class="anotheranimal"></span></u>. but for the fun of the thing he agreed. So the <u><b><span id="onemore"></b></span></u>,
+                    who has consented to act as judge, marked the distance yelled,"
+                    <u><b><span id="quotes"></span></b></u>".
+                </p>
+                <p>
+                    The <u><b><span id="animal"></span></b></u> was soon far out of sight, and to make the <u><b><span id="anotheranimal"></span></b></u> feel very deeply how ridiculous it was for him to try a race with a <u><b><span id="anotheranimal"></span></b></u>.
+                    he went off the course to practise <u><b><span id="verbs"></span></b></u> for
+                    <u><b><span id="num"></span></b></u> hours until the <u><b><span
+                                id="animal"></b></span></u> should catch up.
+                </p>
+                <p>
+                    The <u><b><span id="anotheranimal"></span></b></u> meanwhile kept going slowly but steadily, and, after a time, passed the place where the <u><b><span id="anotheranimal"></span></b></u> was <u><b><span id="verbs"></span></b></u>. The
+                    <u><b><span id="animal"></b></span></u> was so caught up in
+                    <u><b><span id="verbs"></span></b></u>; and when at last he did stop, the <u><b><span id="anotheranimal"></span></b></u> was near the goal. The <u><b><span id="animal"></span></b></u> now ran his swiftest, but he could not overtake
+                    the <u><b><span
+                                id="anotheranimal"></span></b></u> in time.
+                </p>
+                <h2>Moral of the story:</h2>
+                <p><i><span id="msg"></span></i></p>
+            </div>
+        </div>
+    </div>
+    <script src="main.js"></script>
 
+</body>
 
-
-    const msg = document.getElementById("msg").value;
-    localStorage.setItem('msg', msg);
-
-    const decision = document.getElementById("decision").value;
-    localStorage.setItem('decision', decision);
-
-
-
-
-
-    return false;
-}
-
-
-
-
-document.getElementById('num').innerHTML = localStorage.getItem("num");
-
-document.getElementById('decision').innerHTML = localStorage.getItem("decision");
-
-document.getElementById('msg').innerHTML = localStorage.getItem("msg");
-
-document.getElementById('speed').innerHTML = localStorage.getItem("speed");
-
-document.getElementById('quotes').innerHTML = localStorage.getItem("quotes");
-
-
-document.getElementById('adjective').innerHTML = localStorage.getItem("adjective");
-
-document.getElementById('onemore').innerHTML = localStorage.getItem("onemore");
-
-
-
-
-
-var elements = document.querySelectorAll('[id="animal"]');
-for (i = 0; i < elements.length; i++) {
-    elements[i].innerHTML = localStorage.getItem("animal");
-}
-var elem = document.querySelectorAll('[id="anotheranimal"]');
-for (i = 0; i < elem.length; i++) {
-    elem[i].innerHTML = localStorage.getItem("anotheranimal");
-}
-
-var ele = document.querySelectorAll('[id="verbs"]');
-for (i = 0; i < ele.length; i++) {
-    ele[i].innerHTML = localStorage.getItem("verbs");
-}
+</html>
